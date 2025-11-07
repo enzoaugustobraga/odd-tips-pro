175576a8685ecd0344cf0c9668e2e4141f3346e0
enzoaugustobraga <enzodeandradebraga@gmail.com>
2025-11-07 13:12:05 -0300

    Update allowed-emails.ts

--- src/data/allowed-emails.ts
+++ src/data/allowed-emails.ts
@@ -1,8 +1,7 @@
 // Lista de emails autorizados a acessar o sistema
 // Para adicionar um novo email, basta adicioná-lo ao array abaixo
 
-export const allowedEmails = [
-  "enzodeandradebraga@gmail.com",
+export const allowedEmails = process.env.AUTHORIZED_EMAILS?.split(",") || [
   // Adicione mais emails aqui conforme necessário
   // "exemplo@email.com",
 ];
